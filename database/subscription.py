6779bbdb300dbee84169c120d7c35cc616ee8cdc
Ubuntu <ubuntu@ip-172-31-34-68.eu-north-1.compute.internal>
2026-03-31 09:13:55 +0000

    feat: add lost items schema

---
+++ database/subscription.py
@@ -0,0 +1,11 @@
+from database.initdb import db
+from datetime import datetime
+
+class WebPushSubscription(db.Model):
+    __tablename__ = 'webpush_subscriptions'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    subscription_info = db.Column(db.Text, nullable=False)  # Tarayıcıdan gelen JSON verisini string olarak tutar
+    kullanici_ajani = db.Column(db.String(255))             # İsteğe bağlı: Chrome, Safari vb. tarayıcı bilgisini tutmak istersen
+    olusturulma_tarihi = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) # Bildirimin kime ait olduğunu bilmek için
